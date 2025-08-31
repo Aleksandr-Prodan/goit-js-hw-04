326b4906c79ffe6c03858047917765d33e917af0
oleksandr <aleksandrprodan@ukr.net>
2025-08-31 23:10:32 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,14 @@
-// function slugify(title) {
-//     return title.toLowerCase().split(" ").join("-");
-// };
-
-// console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-// console.log(slugify("English for developer")); // "english-for-developer"
-// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript" 
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
 function isEnoughCapacity(products, containerSize) {
-    products це обєкт(назви товарів - значення кількість товарів)
-    containerSize це число(максимальна кількість одиниць товарів)
-    if (condition) {
-        
+    let sum = 0;
+    const value = Object.values(products);
+    
+    for (const element of value) {
+        sum += element;
     }
-    return 
+    if (sum <= containerSize) {
+        return true;
+    }
+    return false;
 }
 
 
