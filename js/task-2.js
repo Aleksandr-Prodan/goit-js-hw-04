91f0cc615a98457eca241a84b3b988f5c7a636ca
oleksandr <aleksandrprodan@ukr.net>
2025-09-01 21:44:01 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -10,8 +10,6 @@ function calcAverageCalories(days) {
     }
 }
 
-
-
 console.log( calcAverageCalories([ { day: "monday", calories: 3010 }, { day:
 "tuesday", calories: 3200 }, { day: "wednesday", calories: 3120 }, { day:
 "thursday", calories: 2900 }, { day: "friday", calories: 3450 }, { day:
--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,16 @@
-// function filterArray(numbers, value) {
-//   const total = [];
-//   for (let i = 0; i < numbers.length; i++) {
-//     if (numbers[i] > value) {
-//       total.push(numbers[i]);
-//     }
-//   }
-//   return total;
-// }
-
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-
-
 const profile = {
   username: "Jacob",
   playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours; 
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
 }
-
-changeUsername(newName)
-updatePlayTime(hours)
-getInfo() `${username} has ${playTime} active hours!,`
-
-
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
